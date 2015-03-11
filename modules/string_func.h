88f221c13d7b4921763765788d388d50749330ff
saroko-dnd <isaroko@mail.ru>
2015-03-11 09:33:14 +0300

    another function with strings

--- modules/string_func.h
+++ modules/string_func.h
@@ -4,5 +4,6 @@
 int string_lenght(const char *);
 int string_comparison(const char *,const char *);
 void string_copy(char *,const char *);
+void string_print_words(const char *,const char *);
 
 #endif // STRING_FUNC_H_INCLUDED
--- modules/string_main.c
+++ modules/string_main.c
@@ -1,4 +1,8 @@
 #define size_max 1000
+#define mas_col 100
+#define mas_str 100
+#include <stdio.h>
+#include <stdlib.h>
 
 int string_lenght(const char *str)
 {
@@ -36,5 +40,106 @@ while (string[counter]!='\0' && counter!=(size_max-1))
 {
     dest_string[counter]=string[counter];
     ++counter;
+}
+}
+
+
+void string_print_words(const char *string,const char *symbols)
+{
+char array[mas_str][mas_col],start,end;
+int str=0,col=0,str_count=0,col_count=0,check=0,counter=0,counter_1=0,counter_2=0,check_point,swi=1;
+//Заполнение двумерного массива пробелами.
+while (str!=mas_str)
+{
+array[str][col]=' ';
+++col;
+if (col==mas_col)
+{
+    col=0;
+    ++str;
+}
+}
+str=0;
+col=0;
+
+while (string[counter]!='\0')
+{
+    while (symbols[counter_1]!='\0')
+    {
+        if (string[counter]==symbols[counter_1])
+        check=1;
+        ++counter_1;
+    }
+counter_1=0;
+    while (check==1)
+    {
+    check=0;
+    while (symbols[counter_1]!='\0')
+    {
+        if (string[counter]==symbols[counter_1])
+        check=1;
+        ++counter_1;
+    }
+    counter_1=0;
+    if (check==1)
+    ++counter;
+    }
+    counter_1=0;
+    check=0;
+    start=string[counter];
+    check_point=counter;
+    while (check==0)
+    {
+        ++counter;
+        check=0;
+    while (symbols[counter_1]!='\0')
+    {
+        if (string[counter]=='\0')
+        check=1;
+        if (string[counter]==symbols[counter_1])
+        check=1;
+        ++counter_1;
+    }
+    counter_1=0;
+    }
+    end=string[counter-1];
+    check=0;
+    //Сверка с таблицей.(двумерным массивом.)
+    if (end==start)
+       {
+           while(array[str][0]!=' ' && str!=mas_str-1 && swi==1)
+           {
+            counter_2=check_point;
+            while(array[str][col]!=' ' && check==0)
+           {
+               if (array[str][col]!=string[counter_2])
+               check=1;
+               ++counter_2;
+               ++col;
+           }
+           if (check==0)
+            swi=0;
+           check=0;
+           col=0;
+           ++str;
+           }
+           str=0;
+           col=0;
+           //Печать
+           if (swi==1)
+           {
+               counter_2=check_point;
+               while (counter_2!=counter)
+               {
+                   printf("%c",string[counter_2]);
+                   array[str_count][col_count]=string[counter_2];
+                   ++counter_2;
+               }
+               col_count=0;
+               ++str_count;
+               printf("\n");
+           }
+           swi=1;
+       }
 }
 }
