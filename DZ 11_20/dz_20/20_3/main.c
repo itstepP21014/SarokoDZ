f8e936326e2d0266cc3c9f6dbec14c868a899886
saroko-dnd <isaroko@mail.ru>
2015-04-25 01:10:05 +0300

    graph

---
+++ DZ 11_20/dz_20/20_3/main.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *file;
+    int vertices,edges,counter=0,first,second,swi=1,top_count=1,begin=0,end=1;
+
+        file=fopen("graf_data.txt","r");
+        if (file==0)
+        {
+            printf("Cant open file!\n");
+            return 1;
+        }
+        fscanf(file,"%d %d",&vertices,&edges);
+        int matrix[vertices][vertices],tops[vertices],result[vertices];
+
+        while (counter<vertices)
+        {
+            tops[counter]=1;
+            ++counter;
+        }
+        counter=0;
+
+        first=0;
+        second=0;
+        while (first<vertices)
+        {
+            while (second<vertices)
+            {
+                matrix[first][second]=0;
+                ++second;
+            }
+            second=0;
+            ++first;
+        }
+
+        printf ("counter top:%d\n",vertices);
+        while (counter<edges)
+        {
+            fscanf(file,"\r\n%d %d",&first,&second);
+            matrix[first-1][second-1]=1;
+            matrix[second-1][first-1]=1;
+            printf ("edges:%d %d\n",first,second);
+            ++counter;
+        }
+        counter=0;
+        fclose(file);
+
+        first=0;
+        second=0;
+        printf("\n");
+        counter=0;
+        first=0;
+        second=0;
+        tops[first]=0;
+        printf("into the deep:%d ",top_count);
+
+        while (swi)
+        {
+            while (swi)
+            {
+                while (matrix[first][second]!=1)
+                {
+                    ++second;
+                }
+                if (tops[second]==1)
+                {
+                    printf("%d ",second+1);
+                    tops[second]=0;
+                    swi=0;
+                }
+                else
+                ++second;
+            }
+            swi=1;
+            first=second;
+            second=0;
+            while (counter<vertices)
+            {
+                if (tops[counter]==1)
+                swi=2;
+                ++counter;
+            }
+            counter=0;
+            if (swi==1)
+            swi=0;
+        }
+
+        printf("\n");
+        file=fopen("graf_data.txt","r");
+        if (file==0)
+        {
+            printf("Cant open file!\n");
+            return 1;
+        }
+        fscanf(file,"%d %d",&vertices,&edges);
+
+        while (counter<vertices)
+        {
+            tops[counter]=1;
+            result[counter]=0;
+            ++counter;
+        }
+        counter=0;
+
+        first=0;
+        second=0;
+        while (first<vertices)
+        {
+            while (second<vertices)
+            {
+                matrix[first][second]=0;
+                ++second;
+            }
+            second=0;
+            ++first;
+        }
+
+        while (counter<edges)
+        {
+            fscanf(file,"\r\n%d %d",&first,&second);
+            matrix[first-1][second-1]=1;
+            matrix[second-1][first-1]=1;
+            printf ("edges:%d %d\n",first,second);
+            ++counter;
+        }
+        counter=0;
+        fclose(file);
+
+        first=0;
+        second=0;
+        printf("\n");
+        counter=0;
+        first=0;
+        second=0;
+        tops[first]=0;
+        printf("\nin width:%d ",top_count);
+        int v_copy=vertices-1;
+        int counter_2=0;
+        counter=0;
+        swi=1;
+        while (swi)
+        {
+            while (second<vertices)
+            {
+                while (matrix[first][second]!=1)
+                {
+                    ++second;
+                }
+                while (counter<vertices)
+                {
+                    if (result[counter]==second+1)
+                    swi=2;
+                    ++counter;
+                }
+                counter=0;
+                if (matrix[first][second]==1 && swi==1)
+                {
+                    result[counter_2]=second+1;
+                    ++end;
+                    printf("%d ",second+1);
+                    ++counter_2;
+                    --v_copy;
+                }
+                else
+                ++second;
+                swi=1;
+            }
+            first=result[begin];
+            ++begin;
+            second=0;
+            if (v_copy==0)
+            swi=0;
+        }
+
+    return 0;
+}
